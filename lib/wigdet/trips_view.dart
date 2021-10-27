dfde608f4dc747e3ef8d9e765f7dac6da4757990
GregBiglione <gregory.biglione81@gmail.com>
2021-10-27 04:01:56 +0200

    Added TripsView

---
+++ lib/wigdet/trips_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TripsView extends StatefulWidget {
+  static const String ROUTE_NAME = "/trips";
+
+  const TripsView({Key? key}) : super(key: key);
+
+  @override
+  _TripsViewState createState() => _TripsViewState();
+}
+
+class _TripsViewState extends State<TripsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
