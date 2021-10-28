ce19d8283dc11b60aa9495656621706b5495f073
GregBiglione <gregory.biglione81@gmail.com>
2021-10-29 00:33:59 +0200

    Added Dismissible in ToDoActivities

--- lib/wigdet/to_do_activities.dart
+++ lib/wigdet/to_do_activities.dart
@@ -1,4 +1,5 @@
 import 'package:dyma_trip/model/activity_model.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ToDoActivities extends StatelessWidget {
@@ -10,7 +11,33 @@ class ToDoActivities extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: activities.length,
-      itemBuilder: (context, i) => Text(activities[i].name),
+      itemBuilder: (context, i) {
+        Activity activity = activities[i];
+        return Container(
+          margin: EdgeInsets.symmetric(horizontal: 10),
+          child: Dismissible(
+            direction: DismissDirection.endToStart,
+            background: Container(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              alignment: Alignment.centerRight,
+              child: Icon(Icons.check_circle, color: Colors.white, size: 30,),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.green[700],
+              ),
+            ),
+            key: ValueKey(activity.id),
+            child: Card(
+              child: ListTile(
+                title: Text(activity.name),
+              ),
+            ),
+            onDismissed: (_) {
+              print("Activity dismissed");
+            },
+          ),
+        );
+      },
     );
   }
 }
